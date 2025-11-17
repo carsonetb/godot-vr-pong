16ce282b5aee3ed1328d6309d92dcb18ce410290
carsonetb <blackhole243243@gmail.com>
2025-11-16 17:58:44 -0800

    Hello

--- game/ball/ball.gd
+++ game/ball/ball.gd
@@ -33,6 +33,7 @@ func _physics_process(_delta: float) -> void:
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if should_override_and_unfreeze:
+		should_override_and_unfreeze = false
 		state.transform.origin = position_to_override
 		state.transform.basis = Basis.from_euler(rotation_to_override)
 		state.linear_velocity = velocity_to_override
